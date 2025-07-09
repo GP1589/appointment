76e6290f0413659e16c90ab6fd6eceb73f2ed52c
Christian Castro Ortiz <krisfb51@gmail.com>
2025-07-09 00:15:53 -0500

    str

--- src/api/app.ts
+++ src/api/app.ts
@@ -7,7 +7,7 @@ import { AppointmentController } from "../application/controllers/AppointmentCon
 import { AppointmentService } from "../application/services/AppointmentService";
 import { AppointmentRepository } from "../infrastructure/repositories/implementation/AppointmentRepository";
 import { createAppointmentRoutes } from "./routes/appointmentRoutes";
-
+import { setupSwagger } from "./swagger";
 
 export class App {
   private app: express.Application;
@@ -16,6 +16,7 @@ export class App {
     this.app = express();
     this.setupDependencies();
     this.setupMiddlewares();
+    this.setupSwagger();
     this.setupRoutes();
     this.setupErrorHandling();
   }
@@ -23,12 +24,11 @@ export class App {
   private appointmentController!: AppointmentController;
 
   private setupDependencies(): void {
-
-
     const appointmentRepository = new AppointmentRepository();
     const appointmentService = new AppointmentService(appointmentRepository);
     const appointmentController = new AppointmentController(appointmentService);
-    this.appointmentController = appointmentController;}
+    this.appointmentController = appointmentController;
+  }
 
   private setupMiddlewares(): void {
     this.app.use(helmet());
@@ -36,6 +36,10 @@ export class App {
     this.app.use(express.json({ limit: "10mb" }));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(requestLogger);
+  }
+
+  private setupSwagger(): void {
+    setupSwagger(this.app);
   }
 
   private setupRoutes(): void {
@@ -65,3 +69,71 @@ export class App {
     return this.app;
   }
 }
+
+// import express from "express";
+// import cors from "cors";
+// import helmet from "helmet";
+// import { errorHandler } from "../api/middlewares/errorHandler";
+// import { requestLogger } from "../api/middlewares/requestLogger";
+// import { AppointmentController } from "../application/controllers/AppointmentController";
+// import { AppointmentService } from "../application/services/AppointmentService";
+// import { AppointmentRepository } from "../infrastructure/repositories/implementation/AppointmentRepository";
+// import { createAppointmentRoutes } from "./routes/appointmentRoutes";
+// import { setupSwagger } from "./swagger";
+
+// export class App {
+//   private app: express.Application;
+
+//   constructor() {
+//     this.app = express();
+//     this.setupDependencies();
+//     this.setupMiddlewares();
+//     this.setupRoutes();
+//     this.setupErrorHandling();
+
+//   }
+
+//   private appointmentController!: AppointmentController;
+
+//   private setupDependencies(): void {
+
+//     const appointmentRepository = new AppointmentRepository();
+//     const appointmentService = new AppointmentService(appointmentRepository);
+//     const appointmentController = new AppointmentController(appointmentService);
+//     this.appointmentController = appointmentController;}
+
+//   private setupMiddlewares(): void {
+//     this.app.use(helmet());
+//     this.app.use(cors());
+//     this.app.use(express.json({ limit: "10mb" }));
+//     this.app.use(express.urlencoded({ extended: true }));
+//     this.app.use(requestLogger);
+//   }
+
+//   private setupRoutes(): void {
+//     this.app.use(
+//       "/appointment",
+//       createAppointmentRoutes(this.appointmentController)
+//     );
+
+//     // Ruta no encontrada (último recurso)
+//     this.app.use("*", (req, res) => {
+//       res.status(404).json({
+//         success: false,
+//         error: {
+//           message: "Endpoint not found",
+//           path: req.originalUrl,
+//           method: req.method,
+//         },
+//       });
+//     });
+//   }
+
+//   private setupErrorHandling(): void {
+//     this.app.use(errorHandler);
+//   }
+
+//   public getApp(): express.Application {
+//     return this.app;
+//   }
+// }
