87292cd0576c3d7a957d71ee20b588f881025c8e
Christian Castro Ortiz <krisfb51@gmail.com>
2025-07-08 19:06:14 -0500

    str

--- src/infrastructure/config/environment.ts
+++ src/infrastructure/config/environment.ts
@@ -2,5 +2,5 @@ export const config = {
   port: process.env.PORT || 5000,
   nodeEnv: process.env.NODE_ENV || 'development',
   awsRegion: process.env.AWS_REGION || 'us-east-1',
-  tableName: process.env.TABLE_NAME || 'chris-dev-users'
+  tableName: process.env.TABLE_NAME || 'appointment',
 };
